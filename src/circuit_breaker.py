1f9375df9e8e2ba750ae7f6bc1f55d300404dba1
Canh Ngo <canhnt@gmail.com>
2022-04-22 23:49:15 +0200

    Implemented circuit-breaker with fixed sleeptime

---
+++ src/circuit_breaker.py
@@ -0,0 +1,50 @@
+from http.client import HTTPResponse
+import time
+
+# static delay in seconds after failure
+SLEEP_TIME = 2
+
+
+class CircuitOpenTimeout(Exception):
+    """ Exception when the circuit got timeout. """
+    pass
+
+
+class CircuitOpenError(Exception):
+    """ Exception when the circuit exceeded maximal error threshold. """
+    pass
+
+
+class CircuitBreaker:
+    """ A circuit breaker wrapper utility to handle retries upon errors or timeout. """
+
+    def __init__(self, http_client, error_threshold: int, time_window: int):
+        self.http_client = http_client
+        self.error_threshold = error_threshold
+        self.time_window = time_window
+
+    def do_request(self, method: str, path: str) -> HTTPResponse:
+        """ Execute request with circuit breaker config"""
+        counter = 0
+
+        start_time = time.time()
+        while counter < self.error_threshold:
+            self.http_client.request(method, path)
+            resp = self.http_client.getresponse()
+            # Only retries upon 5xx or REQUEST_TIMEOUT (408)
+            if resp.status < 500 and resp.status != 408:
+                # flow normally
+                return resp
+
+            ellapsed_time = time.time() - start_time
+            # print(ellapsed_time)
+            if ellapsed_time > self.time_window - SLEEP_TIME:
+                raise CircuitOpenTimeout
+
+            counter += 1
+            time.sleep(SLEEP_TIME)
+        raise CircuitOpenError
+
+
+# if __name__ == "__main__":
+#     breaker = CircuitBreaker(stub_client, 3, 20)
